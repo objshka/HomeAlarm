e9f9d27980f1362983232fd319b2b7a8285f28bb
objshka <yazkiri4@gmail.com>
2023-02-26 23:54:27 +0300

    plsWork

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private RoomTrigger _roomTrigger;
+
+    private Coroutine _corutine;
+    private bool _isPlaying;
+    private float _maxValue = 1f;
+    private float _minValue = 0;
+    private float changeRate = 0.3f;
+
+    private void OnEnable()
+    {
+        _roomTrigger.Entered += OnEntered;
+        _roomTrigger.Left += OnLeft;
+    }
+
+    private void OnDisable()
+    {
+        _roomTrigger.Entered -= OnEntered;
+        _roomTrigger.Left -= OnLeft;
+    }
+
+    private void OnEntered()
+    {
+
+        if (_isPlaying == false)
+        {
+            _audioSource.Play();
+            _isPlaying = true;
+        }
+
+        SetCurrentValue(_maxValue);
+    }
+
+    private void OnLeft()
+    {
+        SetCurrentValue(_minValue);
+    }
+
+    private void SetCurrentValue(float targetVolume)
+    {
+        if (_corutine != null)
+        {
+            StopCoroutine(_corutine);
+        }
+        _corutine = StartCoroutine(ChangeVolume(targetVolume));
+    }
+
+    private IEnumerator ChangeVolume(float targetVolume)
+    {
+
+        while (_audioSource.volume != targetVolume)
+        {
+            _audioSource.volume = Mathf.MoveTowards(_audioSource.volume, targetVolume, changeRate * Time.deltaTime);
+
+            if (_audioSource.volume == _minValue)
+            {
+                _audioSource.Stop();
+                _isPlaying = false;
+            }
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/RoomTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class RoomTrigger : MonoBehaviour
+{
+    public event Action Entered;
+    public event Action Left;
+
+    private void OnTriggerEnter2D(Collider2D collider2D)
+    {
+        if (collider2D.TryGetComponent(out Enemy enemy))
+        {
+            Entered.Invoke();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider2D)
+    {
+        if (collider2D.TryGetComponent(out Enemy enemy))
+        {
+            Left.Invoke();
+        }
+    }
+}
